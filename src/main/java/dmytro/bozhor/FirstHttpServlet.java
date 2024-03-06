689ba4e09f66041aee601da9b7024783915e385a
DmytroBozhor <dimaastop1@gmail.com>
2024-03-06 19:48:51 +0100

    servlet practice

---
+++ src/main/java/dmytro/bozhor/FirstHttpServlet.java
@@ -0,0 +1,21 @@
+package dmytro.bozhor;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/first")
+public class FirstHttpServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+        try (var printWriter = resp.getWriter()) {
+            printWriter.write("<h1>first http servlet<h1>");
+        }
+    }
+
+}
